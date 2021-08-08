6c02c4e0cc743b67bf34e002981896d8b24d92d0
leeviko <lerppana0009@gmail.com>
2021-08-08 15:58:44 +0300

    server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+app.get("/results/:searchVal", (req, res) => {
+  const value = req.params.searchVal;
+  axios.get(`https://imdb-api.com/en/API/Search/${process.env.API_KEY}/${value}`)
+  .then((resp) => {
+    // handle success
+    res.json(resp.data.results)
+  })
+  .catch((err) => {
+    // handle error
+    console.log(err);
+  })
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`)) 
