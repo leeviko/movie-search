606d97c5617b66284772471b24c44e21a0d32fc1
leeviko <lerppana0009@gmail.com>
2021-08-10 15:28:59 +0300

    Get items from api

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { getItems } from "../actions/itemActions";
+import { useDispatch } from "react-redux";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const [searchVal, setSearchVal] = useState(""); 
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getItems(searchVal))
+  }
+
   return (
-    <form method="POST" className="search-form">
-      <input type="text" name="search-value" className="search-field" />         
+    <form onSubmit={(e) => handleSubmit(e)} method="POST" className="search-form">
+      <input 
+        type="text" 
+        name="search-value" 
+        value={searchVal} 
+        onChange={(e) => setSearchVal(e.target.value)} 
+        className="search-field" 
+      />         
       <button type="submit" className="search-btn">Hae</button>
     </form>
   )
